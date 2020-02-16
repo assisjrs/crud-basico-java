21601b8234fff2f570c17f4938d974efd32317e3
Melo, Francisco <francisco.melo@concrete.com.br>
2020-02-16 14:39:08 -0300

    primeiro teste para verificar o banco

---
+++ src/test/java/com/assisjrs/crudjava/SqlEmbeddedTest.java
@@ -0,0 +1,24 @@
+package com.assisjrs.crudjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class SqlEmbeddedTest {
+
+	@Autowired
+	private EntityManager entityManager;
+
+	@Test
+	void deve_iniciar_o_sql_embedded() {
+		final Query query = entityManager.createNativeQuery("SELECT count(*) FROM   INFORMATION_SCHEMA.SYSTEM_TABLES;");
+
+		assertThat(query.getSingleResult()).isNotNull();
+	}
+}
