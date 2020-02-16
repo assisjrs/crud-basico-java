57be7119887b3f06c4f6f2611502420cf11082cb
Melo, Francisco <francisco.melo@concrete.com.br>
2020-02-16 16:25:05 -0300

    controller buscando informações do service

--- src/main/java/com/assisjrs/crudjava/controller/ClienteController.java
+++ src/main/java/com/assisjrs/crudjava/controller/ClienteController.java
@@ -1,4 +1,26 @@
-package com.assisjrs.crudjava;
+package com.assisjrs.crudjava.controller;
 
+import com.assisjrs.crudjava.model.service.ClienteService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
+    private ClienteService service;
+
+    @GetMapping("{id}")
+    public ResponseEntity<ClienteResponse> get(@PathVariable final Long id){
+        return ResponseEntity.ok(mapper.map(service.byId(id), ClienteResponse.class));
+    }
 }
